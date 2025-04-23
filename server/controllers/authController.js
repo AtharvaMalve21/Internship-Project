38036f677eedc78e506d456c129493306f4f11c0
Atharva Malve <160827155+AtharvaMalve21@users.noreply.github.com>
2025-04-23 19:47:43 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -65,7 +65,7 @@ exports.register = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: process.env.NODE_ENV === "production" ? "None" : "strict",
     });
 
     console.log(newUser);
@@ -118,7 +118,7 @@ exports.login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: process.env.NODE_ENV === "production" ? "None" : "strict",
     });
 
     console.log(existingUser);
